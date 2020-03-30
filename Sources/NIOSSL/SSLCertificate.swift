55927d21b72fa39d0aed0854c12301751b74a565
Ethan Jin <toochen@outlook.com>
2020-03-30 21:58:58 +0800

    More public API for generating TLS certificates

--- Sources/NIOSSL/SSLCertificate.swift
+++ Sources/NIOSSL/SSLCertificate.swift
@@ -12,13 +12,13 @@
 //
 //===----------------------------------------------------------------------===//
 
-#if compiler(>=5.1) && compiler(<5.3)
-@_implementationOnly import CNIOBoringSSL
-@_implementationOnly import CNIOBoringSSLShims
-#else
+//#if compiler(>=5.1) && compiler(<5.3)
+//@_implementationOnly import CNIOBoringSSL
+//@_implementationOnly import CNIOBoringSSLShims
+//#else
 import CNIOBoringSSL
 import CNIOBoringSSLShims
-#endif
+//#endif
 import NIO
 
 /// A reference to a BoringSSL Certificate object (`X509 *`).
@@ -31,7 +31,7 @@ import NIO
 /// to obtain an in-memory representation of a TLS certificate from a buffer of
 /// bytes or from a file path.
 public class NIOSSLCertificate {
-    internal let _ref: UnsafeMutableRawPointer/*<X509>*/
+    public let _ref: UnsafeMutableRawPointer/*<X509>*/
 
     internal var ref: UnsafeMutablePointer<X509> {
         return self._ref.assumingMemoryBound(to: X509.self)
@@ -149,7 +149,7 @@ public class NIOSSLCertificate {
     ///
     /// In general, however, this function should be avoided in favour of one of the convenience
     /// initializers, which ensure that the lifetime of the `X509` object is better-managed.
-    static func fromUnsafePointer(takingOwnership pointer: UnsafeMutablePointer<X509>) -> NIOSSLCertificate {
+    public static func fromUnsafePointer(takingOwnership pointer: UnsafeMutablePointer<X509>) -> NIOSSLCertificate {
         return NIOSSLCertificate(withOwnedReference: pointer)
     }
 
